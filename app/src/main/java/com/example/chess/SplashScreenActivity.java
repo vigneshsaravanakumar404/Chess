90f6b3b9a536377e57b51b76aa864808c1172189
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-06-18 19:27:12 -0400

    bad shit gonna happen after this

--- app/src/main/java/com/example/chess/SplashScreenActivity.java
+++ app/src/main/java/com/example/chess/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.chess;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,12 +9,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
 
     TextView textView;
     ProgressBar progressBar;
-    int duration = 2000;
+    int duration = 500;
     int incrementStep = 1;
     long delay = (long) (duration / 100.0);
     Handler handler;
@@ -35,6 +37,10 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         // Make the activity full screen
         getWindow().getDecorView().setSystemUiVisibility(5894);
+        handler.postDelayed(runnable, delay);
+        textView.setText("Chess\nBy: Vignesh");
+        textView.setGravity(1);
+        getWindow().getDecorView().setBackgroundColor(0xFF00FF00);
 
 
         runnable = new Runnable() {
@@ -53,22 +59,16 @@ public class SplashScreenActivity extends AppCompatActivity {
                 if (progress < 100) {
                     // Post the next update with the calculated delay
                     handler.postDelayed(this, delay);
+                } else {
+                    // Progress bar has finished, start the new activity
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
                 }
             }
         };
         handler.postDelayed(runnable, delay);
-        textView.setText("Chess\nBy: Vignesh");
-        textView.setGravity(1);
-        getWindow().getDecorView().setBackgroundColor(0xFF00FF00);
-        Thread thread = new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        thread.start();
+
+// ...
 
 
     }
